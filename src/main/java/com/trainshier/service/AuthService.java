743929cd13cb87aa6aeffa26b7fd5e80884eefc5
miguellopez242005 <miguellopez242005@gmail.com>
2026-06-22 23:25:07 -0500

    Revert "Fix:RFID"

--- src/main/java/com/trainshier/service/AuthService.java
+++ src/main/java/com/trainshier/service/AuthService.java
@@ -66,7 +66,6 @@ public class AuthService {
         userRepository.save(user);
 
         MessageResponseDTO response = new MessageResponseDTO();
-
         response.setMessage("User registered successfully");
 
         return response;
@@ -149,7 +148,6 @@ public class AuthService {
      * @return response message
      */
     public MessageResponseDTO recoverPassword(RecoverPasswordRequestDTO request) {
-
         Optional<User> optionalUser =
                 userRepository.findByEmail(request.getEmail());
 
@@ -163,6 +161,7 @@ public class AuthService {
         user.setPassword(encryptedPassword);
         userRepository.save(user);
 
+        // Simulate sending password recovery email
         System.out.println("==================================================");
         System.out.println("📨 CORREO ELECTRÓNICO ENVIADO (SIMULADO)");
         System.out.println("Destinatario: " + user.getEmail());
@@ -176,7 +175,6 @@ public class AuthService {
         MessageResponseDTO response = new MessageResponseDTO();
         response.setMessage("Contraseña restablecida y guardada correctamente. Se envió confirmación al correo.");
         return response;
-
     }
 
     /**
